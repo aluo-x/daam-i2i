40d90c13540a0325d09a13898961fd006309c245
RishiDarkDevil <rishi8001100192@gmail.com>
2023-05-03 03:25:32 +0530

    Comments o faster convergence

--- daami2i/heatmap.py
+++ daami2i/heatmap.py
@@ -301,7 +301,8 @@ class GlobalHeatMap:
 
             # Iterating
             for _ in tqdm(range(n_iter)):
-                # Aggregating all the heatmaps
+                # Aggregating all the heatmaps (Maybe conv_weight as input image instead of heat_maps2d could give faster convergence?
+                # Need to use conv_weight corresponding to the updated heatmaps then)
                 conv_weight = F.conv2d(heat_maps2d, conv_weight)[0].view(self.heat_maps.shape)[:, :, None, None]
                 # Cut off noisy values based on threshold
                 conv_weight[conv_weight < (torch.min(conv_weight, 1, keepdims=True)[0] + thresh)] = 0
